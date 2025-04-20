942b596d50debd870472d80de1f31953ad0f5d42
Shubhz21 <shubhamfunde2222@gmail.com>
2025-04-20 11:27:11 +0530

    newcmt

--- Employee-management/src/main/java/com/example/demo/entity/Employee.java
+++ Employee-management/src/main/java/com/example/demo/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,9 +8,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "employees")
+
 public class Employee {
 	
 	@Id
